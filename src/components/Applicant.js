797a6d8f98dbbbb7dbf6461aaec3b69e0721fc70
Nick Lee <nicklee808@gmail.com>
2018-07-15 19:54:02 -1000

    Applicant form added

--- src/components/Applicant.js
+++ src/components/Applicant.js
@@ -4,7 +4,72 @@ class Applicant extends Component {
   render(){
     return (
       <div className="Applicant-component">
-        <h2>Applicants</h2>
+        <h2>Applicant</h2>
+        <form>
+          <div className="Applicant-name">
+            Applicant name:
+            <input type="text" name="name"/>
+          </div>
+          <div className="Applicant-attributes">
+            Strength:
+            <select name="strength">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Dexterity:
+            <select name="dexterity">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Intelligence:
+            <select name="intelligence">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Charisma:
+            <select name="charisma">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <input type="submit" value="Submit Applicant"/>
+        </form>
       </div>
     )
   }
