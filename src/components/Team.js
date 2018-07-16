8f4c7b8d1b48de2f3222ef734574236778ff5f60
Nick Lee <nicklee808@gmail.com>
2018-07-15 19:52:48 -1000

    Team form added

--- src/components/Team.js
+++ src/components/Team.js
@@ -5,6 +5,71 @@ class Team extends Component {
     return (
       <div className="Team-component">
         <h2>Team</h2>
+        <form>
+          <div className="Team-name">
+            Team member name:
+            <input type="text" name="name"/>
+          </div>
+          <div className="Team-attributes">
+            Strength:
+            <select name="strength">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Dexterity:
+            <select name="dexterity">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Intelligence:
+            <select name="intelligence">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+            Charisma:
+            <select name="charisma">
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+          <input type="submit" value="Submit Team Member"/>
+        </form>
       </div>
     )
   }
